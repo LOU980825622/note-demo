7676c7590389ad9e224edf59141705a8643fc53d
louyuanlong <louyuanlong@iyunxiao.com>
2018-05-26 17:34:20 +0800

    笔记应用左侧导航栏样式

--- src/components/note/noteIndex.vue
+++ src/components/note/noteIndex.vue
@@ -1,34 +1,38 @@
 <template>
     <div class="note">
-        <p v-for="(item,index) in title" :key="index">{{item}}</p>
         <ul class="note-left">
-            <li v-for="(items,index) in leftIcon" :key="index">
-            <!-- :style="{ backgroundImage , url(items[index].image) }"> -->
-                <!-- <img :src="items[index].image" alt=""> -->
-                <!-- <p>{{items[index].text}}</p> -->
+            <li v-for="(items,index) in leftIcon" :key="index"
+             :title="items.text" @click="leftBtn(index)">
+                <div :style="{backgroundImage:'url('+items.image+')'}"></div>
             </li>
         </ul>
     </div>
 </template>
 <script>
+import leftImage1 from '../../../static/images/add.png'
+import leftImage2 from '../../../static/images/collection.png'
 export default {
     data(){
         return {
-            title:['这是笔记应用1','这是笔记应用2','这是笔记应用3'],
             leftIcon:[
                 {
-                    // image:'../../../static/add.png',
-                    text:1
+                    image:leftImage1,
+                    text:'添加'
                 },
                 {
-                    // image:'../../../static/collection.png',
-                    text:2
+                    image:leftImage2,
+                    text:'收藏'
                 },
                 {
-                    // image:'../../../static/add.png',
-                    text:3
+                    image:leftImage1,
+                    text:'删除'
                 }
             ]
+        }
+    },
+    methods:{
+        leftBtn(n){
+            console.log(n)
         }
     }
 }
@@ -50,14 +54,24 @@ export default {
             width: 80px;
             height: 100%;
             background-color: #25323c;
+            padding-top: 30px;
+            box-sizing: border-box;
 
             li {
                 width: 30px;
                 height: 30px;
-                background: {
-                    color: white;
-                    position: center;
-                    size: cover;
+                margin: 0 auto 30px;
+
+                div {
+                    height: 30px;
+                    background: {
+                        position: center;
+                        size: cover;
+                    }
+                }
+
+                &:last-child {
+                    transform: rotate(45deg);
                 }
             }
         }
